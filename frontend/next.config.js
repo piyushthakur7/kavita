08d6f63aebf85863b372e523231b32f7e269475b
Piyush <tpiyush300@gmail.com>
2026-05-12 10:48:35 +0530

    chore: update package-lock.json dependencies

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,6 +1,9 @@
+const path = require('path');
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  outputFileTracingRoot: path.join(__dirname, '../'),
   eslint: {
     // Warning: This allows production builds to successfully complete even if
     // your project has ESLint errors.
